6d26b4f7231aef425d86f59697674de09b9e3be6
Matthew Falbo <matthew.falbo@gmail.com>
2014-05-07 11:15:11 -0600

    Completed Exercise.  Homepage sends a submit form that takes an email.  When an email is submitted user is redirected to a success page.

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send('<h1>Hello Boulder</h1>');
+	res.send('<form method="post" action="/formsubmit"><input name="email" type="email"><input type="submit"></form>');
+});
+
+app.post('/formsubmit', function(req,res){
+	res.redirect('/success');
+});
+
+app.get('/success', function(req, res){
+	res.send('<h1>Success!</h1>');
 });
 
 var server = app.listen(8932, function() {
